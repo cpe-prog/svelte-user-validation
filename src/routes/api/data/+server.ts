0c7af1fee59696f3543c04c9d2003209f589dac3
Grian Gajila <griangajila2@gmail.com>
2024-05-04 01:29:17 +0800

    wip

--- src/routes/api/data/+server.ts
+++ src/routes/api/data/+server.ts
@@ -1,11 +1,43 @@
 import { json } from '@sveltejs/kit';
 
 export const GET = async () => {
-	const data = {
-		title: 'My Report',
-		content: 'This is a sample report generated with jsPDF in SvelteKit.',
-		date: new Date().toISOString()
-	};
+	const data = [
+		{
+			id: 1,
+			firstName: 'John',
+			lastName: 'Doe',
+			age: 32,
+			status: 'Male'
+		},
+		{
+			id: 2,
+			firstName: 'John2',
+			lastName: 'Doe',
+			age: 32,
+			status: 'Male'
+		},
+		{
+			id: 3,
+			firstName: 'John3',
+			lastName: 'Doe',
+			age: 32,
+			status: 'Male'
+		},
+		{
+			id: 4,
+			firstName: 'John4',
+			lastName: 'Doe',
+			age: 32,
+			status: 'Male'
+		},
+		{
+			id: 5,
+			firstName: 'John5',
+			lastName: 'Doe',
+			age: 32,
+			status: 'Male'
+		}
+	];
 
 	return json(data);
 };
