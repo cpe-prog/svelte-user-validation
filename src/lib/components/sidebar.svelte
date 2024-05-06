a5db652843566d924c544605406c29f3cd6129eb
Grian Gajila <griangajila2@gmail.com>
2024-05-06 13:22:26 +0800

    wip

--- src/lib/components/sidebar.svelte
+++ src/lib/components/sidebar.svelte
@@ -2,20 +2,11 @@
 	export let open = false;
 </script>
 
-<aside class="absolute h-full w-56 border-r-2 bg-gray-200 shadow-lg" class:open>
+<aside
+	class={`absolute h-full w-56 border-r-2  shadow-lg transition-all duration-300 ${open ? 'left-[0]' : 'left-[-100%]'}`}
+>
 	<nav class="p-12 text-xl">
 		<a class="block" href="#about">About</a>
 		<a class="block" href="#contact">Contact</a>
 	</nav>
 </aside>
-
-<style>
-	aside {
-		left: -100%;
-		transition: left 0.3s ease-in-out;
-	}
-
-	.open {
-		left: 0;
-	}
-</style>
--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
+	import Navbar from '$lib/components/navbar.svelte';
+	import Sidebar from '$lib/components/sidebar.svelte';
+
+	let open = false;
 </script>
 
 <div>
 	<header>
-		<!-- <Header /> -->
+		<Navbar bind:sidebar={open} />
+		<Sidebar bind:open />
 	</header>
 	<main>
 		<slot />
