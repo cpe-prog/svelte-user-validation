e7fc6b704b46348a1f727a270048621150602388
Grian Gajila <griangajila2@gmail.com>
2024-05-07 01:08:39 +0800

    wip

--- src/routes/dashboard/generate-pdf.svelte
+++ src/routes/dashboard/generate-pdf.svelte
@@ -1,12 +1,38 @@
 <script lang="ts">
 	import Button from '$lib/components/ui/button/button.svelte';
-	import jsPDF from 'jspdf';
-	import autoTable from 'jspdf-autotable';
+	import type { Template } from '@pdfme/common';
+	import { generate } from '@pdfme/generator';
 	import { Loader } from 'lucide-svelte';
 	import { onMount } from 'svelte';
+	import { Toaster, toast } from 'svelte-sonner';
 
 	let data: { firstName: string; lastName: string; age: number; status: string };
 
+	const template: Template = {
+		basePdf: 'https://pdfme.com/sample.pdf',
+		schemas: [
+			{
+				firstName: {
+					type: 'text',
+					position: { x: 0, y: 0 },
+					width: 10,
+					height: 10
+				},
+				lastName: {
+					type: 'text',
+					position: { x: 10, y: 10 },
+					width: 10,
+					height: 10
+				}
+			}
+		]
+	};
+	const inputs = [
+		{
+			firstName: 'sample',
+			lastName: 'sample'
+		}
+	];
 	onMount(async () => {
 		try {
 			const response = await fetch('/api/data');
@@ -18,36 +44,27 @@
 			console.error('Error fetching data:', err);
 		}
 	});
-	function generatePDF() {
-		const doc = new jsPDF();
-		doc.text('Hello world!', 0, 10);
-		doc.setFontSize(20);
-		doc.text('User Data', 10, 20);
-		doc.setFontSize(12);
 
-		autoTable(doc, {
-			theme: 'striped',
-			styles: {
-				font: 'helvetica',
-				lineColor: [0, 0, 0]
-			},
-			tableWidth: 100,
-			margin: { top: 30, left: 30 },
-			head: [['Name', 'age', 'status']],
-			body: [[`${data.firstName + ' ' + data.lastName}`, `${data.age}`, `${data.status}`]]
-		});
-		const blobPDF = new Blob([doc.output('blob')], { type: 'application/pdf' });
-		const blobUrl = URL.createObjectURL(blobPDF);
-		window.open(blobUrl, '_blank');
+	async function generatePdf() {
+		try {
+			const pdf = await generate({ template, inputs });
+			const blob = new Blob([pdf.buffer], { type: 'application/pdf' });
+			const url = URL.createObjectURL(blob);
+			window.open(url);
+			toast.success('Pdf Generated successfully!');
+		} catch (error) {
+			toast.error('Cannot generate pdf!');
+		}
 	}
 </script>
 
 {#if data}
+	<Toaster expand={false} richColors position="bottom-right" />
 	<h1>Name: {data.firstName + ' ' + data.lastName}</h1>
 	<p>Age: {data.age}</p>
 	<p>Status: {data.status}</p>
 
-	<Button on:click={generatePDF}>Download PDF</Button>
+	<Button on:click={generatePdf}>Download PDF</Button>
 {:else}
 	<Loader class="animate-spin" />
 	<p>Loading data...</p>
